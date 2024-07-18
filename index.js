8db7b4a29e47f6914f5c54e76501094c28f5ec0e
Duncan Kipkemoi <60089273+flawlesscode254@users.noreply.github.com>
2024-07-18 14:22:17 +0300

    Done

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const Commits = require("./routes/Commits");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(helmet());
+app.use(morgan("common"));
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Entry file",
+  });
+});
+
+app.use("/commits", Commits);
